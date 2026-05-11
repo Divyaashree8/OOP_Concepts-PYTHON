022710057f113cb1eb8a3abec198c17940e9c4d0
Divyaashree8 <divyaashree8@gmail.com>
2026-05-11 17:12:22 +0530

    Implemented abstraction using ABC and enforced withdraw method in class classes

--- oops-concept.py
+++ oops-concept.py
@@ -1,37 +1,37 @@
-class BankAccount:
+from abc import ABC, abstractmethod
+class Account(ABC):#abtract base class
   def __init__(self,account_holder,account_number,balance):
     self.account_holder=account_holder
     self.account_number=account_number
-    self.__balance=balance #private variable
-  def get_balance(self):
-    return self.__balance
+    self._balance=balance #private variable
+
   def deposite(self,amount):
      if amount>0:
         self.__balance +=amount
         print(f"${amount} deposited successfully.")
      else:
        print("Invaild amount! Deposite must be positive.")
-  def withdraw(self,amount):
-    print("Withdraw method shoukd be implemented by subclass")
-  
   def display_details(self):
-     print("Account Details)
+     print("\n---Account Details---)
      print(f"Account Holder:{self.account_holder}")
      print(f"Account Number:{self.account_number}")
-     print(f"Balance:${self.__balance}")
+     print(f"Balance:${self._balance}")
+  @abstractmethod
+  def withdraw(self,amount):
+    pass
   # child class
   class SavingsAccount(Account):
     MIN_Balance=1000
     def withdraw(self,amount):
     if amount <=0:
       print("Invalid amount! withdrawal must be positive")
-    elif self.__balance - amount<self.MIM_Balance:
-      print("Cannot withdraw minimum balance must be maintained.")
-    elif amount> self.__balance:
-      print("Insufficient balance")  
+    elif amount> self._balance:
+      print("Insufficient balance")
+    elif self._balance - amount<self.MIM_Balance:
+      print("Cannot withdraw, minimum balance must be maintained.") 
     else:
-      self.__balance-=amount
-      print(f"${amount}withdrawn successfully.")
+      self._balance-=amount
+      print(f"${amount}withdrawn successfully from savings ac.")
 # child class 2
 class CurrentAccount(Account):
     Overdraft_limit=5000
@@ -42,12 +42,12 @@ class CurrentAccount(Account):
       print("Overdraft limit exceeded!")  
     else:
       self.__balance-=amount
-      print(f"${amount}withdrawn successfully.")
-# Polymorphism
-accounts=[
-SavingsAccount("Divya",12345,500000),
-CurrentAccount("Shree",67895,500000)
-]
-for account in accounts:
-  account.display_details()
-  account.withdraw(500)
+      print(f"${amount}withdrawn successfully from current ac.")
+
+s=SavingsAccount("Divya",12345,500000),
+c=CurrentAccount("Shree",67895,500000)
+s.display_details()
+s.withdraw(2000)
+c.diplay_details()
+c.withdraw(5000)
+
